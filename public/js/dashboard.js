9394ee3d2247694b33c56b58925c871b66daaf6b
Abel Linas <linasabel@gmail.com>
2025-01-01 16:30:54 +0000

    Adjusted some styling

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,12 +1,2 @@
-// // Handling rich text editor
-// const buttons = document.querySelectorAll('.btn');
-
-// buttons.forEach(button =>{
-//     button.addEventListener('click', () =>{
-//         let command = button.dataset['element'];
-
-//         document.execCommand(command,false,null);
-//     })
-// })
 
 
